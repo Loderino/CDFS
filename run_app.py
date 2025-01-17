40a02f7e9f860017e4c576fdfc4082b4f3c4608c
k.savostin <k.savostin@inventos.ru>
2025-01-17 16:54:20 +0300

    feat: decompose notice_method

--- run_app.py
+++ run_app.py
@@ -15,35 +15,74 @@ vdbh = VDBHandler()
 v = Vectorizer()
 tagger = Tagger()
 
-def notice_method(event_type, src_path, dest_path):
-    if event_type == EventType.CREATED:
-        file_id = dbh.new_file(Path(src_path).absolute())
-        print(f"запись о файле {Path(src_path).absolute()} создана с id {file_id}")
-        if file_id:
-            tags = tagger.generate_desc(src_path)
-            file_vector = v.get_set_vector(tags, aggregation_method="mean")
-            file_vector.normalize()
-            vdbh.add_vector(file_id, file_vector.value)
-    elif event_type == EventType.DELETED:
-        file_id = dbh.delete_file(Path(src_path).absolute())
-        if file_id:
-            vdbh.remove_vector(file_id)
-            print(f"запись о файле {Path(src_path).absolute()} удалена")
-    elif event_type == EventType.MODIFIED:
-        file_id = dbh.update_file(Path(src_path).absolute(), Path(dest_path).absolute())
-        vdbh.remove_vector(file_id)
-        tags = tagger.generate_desc(src_path)
+
+def handle_new_file(path) -> None:
+    """
+    Processes a newly created file.
+
+    Args:
+        path (str): The full path of the new file.
+    """
+    file_id = dbh.new_file(path)
+    if file_id:
+        print(f"запись о файле {path} создана с id {file_id}")
+        tags = tagger.generate_desc(path)
         file_vector = v.get_set_vector(tags, aggregation_method="mean")
         file_vector.normalize()
         vdbh.add_vector(file_id, file_vector.value)
-        print(f"запись о файле {Path(src_path).absolute()} изменена")
+
+def handle_removed_file(path):
+    """
+    Processes a removed file.
+
+    Args:
+        path (str): The full path of the removed file.
+    """
+    file_id = dbh.delete_file(path)
+    if file_id:
+        vdbh.remove_vector(file_id)
+        print(f"запись о файле {path} удалена")
+
+def handle_updated_file(src_path, dest_path):
+    """
+    Processes an updated file by updating its path in the database and refreshing its vector representation.
+
+    Args:
+        src_path (str): The original path of the file.
+        dest_path (str): The new path of the file after the update.
+    """
+    file_id = dbh.update_file(src_path, dest_path)
+    vdbh.remove_vector(file_id)
+    tags = tagger.generate_desc(dest_path)
+    file_vector = v.get_set_vector(tags, aggregation_method="mean")
+    file_vector.normalize()
+    vdbh.add_vector(file_id, file_vector.value)
+    print(f"запись о файле {Path(src_path).absolute()} изменена")
+
+def handle_moved_file(src_path, dest_path):
+    """
+    Handles the event of a file being moved by updating its path in the database.
+
+    Args:
+        src_path (str): The original path of the file before it was moved.
+        dest_path (str): The new path of the file after it has been moved.
+    """
+    dbh.update_file(src_path, dest_path)
+    print(f"запись о файле {Path(src_path).absolute()} перемещена")
+
+def notice_method(event_type, src_path, dest_path):
+    if event_type == EventType.CREATED:
+        handle_new_file(Path(src_path).absolute())
+    elif event_type == EventType.DELETED:
+        handle_removed_file(Path(src_path).absolute())
+    elif event_type == EventType.MODIFIED:
+        handle_updated_file(Path(src_path).absolute(), Path(dest_path).absolute())
     elif event_type == EventType.MOVED:
-        dbh.update_file(Path(src_path).absolute(), Path(dest_path).absolute())
-        print(f"запись о файле {Path(src_path).absolute()} перемещена")
+        handle_moved_file(Path(src_path).absolute(), Path(dest_path).absolute())
 
 fw = FileSystemWatcher(notice_method)
 
-for ind,file in enumerate(fw.get_current_files()):
+for ind, file in enumerate(fw.get_current_files()):
     file_id = dbh.new_file(file)
     if file_id is not None:
         print(file_id, file)

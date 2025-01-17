9beec1814462644412d01a1cce3703ec38f1dabc
k.savostin <k.savostin@inventos.ru>
2025-01-17 20:20:42 +0300

    feat: Add new vector aggregation functions

--- VectorHandle/__init__.py
+++ VectorHandle/__init__.py
@@ -1,5 +1,9 @@
-from utils.aggregation import mean_method
+from utils.aggregation import mean_method, max_pool_method, min_pool_method, median_pooling, sum_method
 
 NAME_TO_FUNC = {
-            "mean": mean_method
+            "mean": mean_method,
+            "max": max_pool_method,
+            "min": min_pool_method,
+            "median": median_pooling,
+            "sum": sum_method
         }
--- utils/aggregation.py
+++ utils/aggregation.py
@@ -13,3 +13,65 @@ def mean_method(vectors: list[Vector]) -> Vector:
         np.ndarray: The mean vector calculated from the input vectors.
     """
     return Vector(np.mean([vector.value for vector in vectors], axis=0))
+
+def max_pool_method(vectors: list[Vector]) -> Vector:
+    """
+    Computes the maximum vector from a list of vectors using max pooling.
+
+    Args:
+        vectors (List[Vector]): A list of Vector objects.
+
+    Returns:
+        Vector: The maximum vector calculated from the input vectors.
+
+    Raises:
+        ValueError: If the input list is empty.
+    """
+    vector_array = np.array([vector.value for vector in vectors])
+    max_pooled = np.max(vector_array, axis=0)
+
+    return Vector(max_pooled)
+
+def min_pool_method(vectors: list[Vector]) -> Vector:
+    """
+    Computes the minimal vector from a list of vectors using min pooling.
+
+    Args:
+        vectors (List[Vector]): A list of Vector objects.
+
+    Returns:
+        Vector: The minimum vector calculated from the input vectors.
+    """
+    vector_array = np.array([vector.value for vector in vectors])
+    min_pooled = np.min(vector_array, axis=0)
+
+    return Vector(min_pooled)
+
+def median_pooling(vectors: list[Vector]) -> Vector:
+    """
+    Computes the median vector from a list of vectors.
+
+    Args:
+        vectors (List[Vector]): A list of Vector objects.
+
+    Returns:
+        Vector: The median vector calculated from the input vectors.
+    """
+    vector_array = np.array([vector.value for vector in vectors])
+    median_vector = np.median(vector_array, axis=0)
+
+    return Vector(median_vector)
+
+def sum_method(vectors: list[Vector]) -> Vector:
+    """
+    Computes the sum of a list of vectors.
+
+    Args:
+        vectors (list[Vector]): A list of Vector objects to be summed.
+
+    Returns:
+        Vector: The summed vector calculated from the input vectors.
+    """
+    vector_array = np.array([v.value for v in vectors])
+    summed_vector = np.sum(vector_array, axis=0)
+    return Vector(summed_vector)
